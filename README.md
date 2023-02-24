e6c31f5695fc855bd22906ab5e0be84205923d6e
kermage <genealyson.torcende@gmail.com>
2023-02-24 21:09:11 +0800

    update examples with test

--- README.md
+++ README.md
@@ -6,10 +6,30 @@
 
 use ThemePlate\Hook;
 
-Hook::append( 'sample_filter', 'test' );
-Hook::append( 'sample_filter', 'test2' );
-Hook::append( 'sample_filter', 'test3' );
-Hook::prepend( 'sample_filter', 'test0' );
-Hook::pluck( 'sample_filter', 'test2' );
-Hook::replace( 'sample_filter', 'test', 'try' );
+// Hook::<ACTION>( <TAG>, <VALUE> )
+Hook::append( 'sample_filter', 'ing' );
+Hook::prepend( 'sample_filter', 're-' );
+Hook::return( 'sample_filter', 'this!' );
+Hook::pluck( 'sample_filter', 'i' );
+Hook::replace( 'sample_filter', '!', '...' );
+Hook::insert( 'sample_filter', 'u', 2 );
+Hook::once( 'sample_filter', array( 'pluck', 'th' ) );
+```
+
+> ^Line by line output with initial value **test**
+
+| ACTION  | VALUE          | RESULT #1  | RESULT #2  |
+|---------|----------------|------------|------------|
+|  append | ing            |    testing |    testing |
+| prepend | re-            | re-testing | re-testing |
+|  return | this!          |      this! |      this! |
+|   pluck | i              |       ths! |       ths! |
+| replace | ["!","..."]    |     ths... |     ths... |
+|  insert | ["u",2]        |    thus... |    thus... |
+|    once | ["pluck","th"] |      us... |    thus... |
+
+```php
+// apply_filters( <TAG>, <INITIAL> );
+apply_filters( 'sample_filter', 'test' ); // <RESULT #1>
+apply_filters( 'sample_filter', 'test' ); // <RESULT #2>
 ```
--- tests/HookTest.php
+++ tests/HookTest.php
@@ -11,7 +11,9 @@ use Error;
 use PHPUnit\Framework\TestCase;
 use ThemePlate\Hook;
 use ThemePlate\Hook\Handler;
+use ThemePlate\Hook\Helper;
 use function Brain\Monkey\Filters\expectAdded;
+use function Brain\Monkey\Functions\expect;
 
 class HookTest extends TestCase {
 	public const ARGS = array(
@@ -19,6 +21,8 @@ class HookTest extends TestCase {
 		'value',
 		'extra',
 	);
+
+	protected static string $filter_output = 'test';
 
 	protected function setUp(): void {
 		parent::setUp();
@@ -77,5 +81,71 @@ class HookTest extends TestCase {
 		$args = array_values( (array) array_filter( $args ) ); // Cleanup
 
 		call_user_func_array( array( Hook::class, $action ), $args );
+	}
+
+	public function for_readme_sample(): array {
+		return array(
+			array(
+				'append',
+				'ing',
+			),
+			array(
+				'prepend',
+				're-',
+			),
+			array(
+				'return',
+				'this!',
+			),
+			array(
+				'pluck',
+				'i',
+			),
+			array(
+				'replace',
+				array( '!', '...' ),
+			),
+			array(
+				'insert',
+				array( 'u', 2 ),
+			),
+			array(
+				'once',
+				array( 'pluck', 'th' ),
+			),
+		);
+	}
+
+	/** @dataProvider for_readme_sample */
+	public function test_readme_sample( string $action, $value ): void {
+		$second_filter_output = self::$filter_output;
+
+		if ( 'once' === $action ) {
+			expect( 'current_filter' )->withNoArgs()->andReturn( '' );
+
+			$priority = 10;
+			$value    = compact( 'value', 'priority' );
+		}
+
+		self::$filter_output = ( new Handler( $value ) )->$action( self::$filter_output );
+
+		if ( 'once' === $action ) {
+			$value = array_values( $value['value'] );
+		} else {
+			$second_filter_output = self::$filter_output;
+		}
+
+		$output = array(
+			$action,
+			Helper::stringify( $value ),
+			self::$filter_output,
+			$second_filter_output,
+		);
+
+		$mask = "| %7s | %-14s | %10s | %10s |\n";
+
+		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		printf( $mask, ...$output );
+		$this->assertIsString( self::$filter_output );
 	}
 }
